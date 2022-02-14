701c24a221ec523865261708ad1bc46711a2a0ae
Rana Arju <arjurana530@gmail.com>
2022-02-14 17:56:37 +0600

    after complate full dynamic shopping cart

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function  updateCaseNumber(product, price, isIncreasing) {
-    const caseInput = document.getElementById(product + "-number");
-    let inputValue = parseInt(caseInput.value);
+function  updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + "-number");
+    let inputValue = parseInt(productInput.value);
     if (isIncreasing) {
         inputValue = inputValue + 1;
     }
@@ -8,25 +8,40 @@ function  updateCaseNumber(product, price, isIncreasing) {
 
         inputValue = inputValue - 1;
     }
-    caseInput.value = inputValue;
+    productInput.value = inputValue;
     // update Total case price
    
-    const casePrice = document.getElementById(product + '-price');
-    casePrice.innerText = inputValue * price;
-    // return inputValue;
+    const productPrice = document.getElementById(product + '-price');
+    productPrice.innerText = inputValue * price;
+    calculateSubTotal();
+};
+function getInputValue(procuct) {
+     const procuctInput = document.getElementById(procuct + "-number");
+    const procuctNumber = parseInt(procuctInput.value);
+    return procuctNumber;
 }
+function calculateSubTotal() {
+    const phoneTotal = getInputValue('phone') * 1200;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    document.getElementById("sub-total").innerText = subTotal;
+    const taxAmount = subTotal / 10;
+    document.getElementById('tax').innerText = taxAmount;
+    document.getElementById('total-price').innerText = subTotal - taxAmount;
+};
+
 // phone
 document.getElementById("phone-plus").addEventListener("click", function(){
-    updateCaseNumber("phone",1200, true);
+    updateProductNumber("phone",1200, true);
 });
 document.getElementById("phone-minus").addEventListener("click", function(){
-   updateCaseNumber("phone",1200, false);
+   updateProductNumber("phone",1200, false);
 });
 // handle case 
 document.getElementById("case-plus").addEventListener("click", function(){
-  updateCaseNumber("case",59 , true);
+  updateProductNumber("case",59 , true);
     
 });
 document.getElementById("case-minus").addEventListener("click", function(){
-    updateCaseNumber("case", 59, false);
+    updateProductNumber("case", 59, false);
 });
